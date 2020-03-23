6a889e0efb0ae1b0e2d652aeb281d604a9844fbc
pro-gramming <dineshneerajb03@gmail.com>
2020-03-23 20:21:57 +0530

    RanMyFirstPython3OnLinux

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,23 @@
 hw="hello world "
 n=int(input("Enter a No. to print the hello world that times"))
-print(hw*n)
+    
+if n <= 3:
+    print(hw*n)
+else:
+    print("runProgAgain", end=" ")
 
 def good():
     print("good")
 
 def name():
-    print("dinesh")
+    return("dinesh")
+
+st = input("Enter good or name to see String inside it")
+
+if st == "good" :
+    good()
+elif st == "name":
+    print(name())
+else:
+    print("Enter only any of above two to proceed")
+    
